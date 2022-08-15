d8073d6a7e10b35fb2a47f4a08b49ff3e771411e
shashankseth040 <86897442+shashankseth040@users.noreply.github.com>
2022-08-16 04:47:37 +0530

    Delete sakila.sql

--- sakila.sql
+++
@@ -1,6 +0,0 @@
-use sakila;
-select * from actor;
-select actor_id, first_name, last_name from actor where actor_id in (5,7);
-select actor_id, first_name, last_name from actor where actor_id between 5 and 7;
-select actor_id, first_name, last_name from actor where actor_id > 5 and actor_id < 7;
-
